0002137659a8039fccf1c5e4f504e67b39d4dfd1
InstantlyMoist <kyllian007@gmail.com>
2020-10-23 19:13:41 +0200

    fixed issue regarding 1.16.3

--- src/main/java/me/kyllian/xRay/tasks/ChunkTask.java
+++ src/main/java/me/kyllian/xRay/tasks/ChunkTask.java
@@ -37,10 +37,11 @@ public class ChunkTask implements Task {
             public void run() {
                 for (Object object : toRestore) {
                     Chunk chunk = (Chunk) object;
-                    player.getWorld().refreshChunk(chunk.getX(), chunk.getZ());
+                    player.getWorld().unloadChunk(chunk);
+                    player.getWorld().loadChunk(chunk);
                 }
             }
-        }.runTaskAsynchronously(plugin);
+        }.runTask(plugin);
     }
 
     @Override
@@ -70,61 +71,4 @@ public class ChunkTask implements Task {
         }
         return newChunks;
     }
-
-    /*private XRayPlugin plugin;
-    private Player player;
-
-    private ArrayList<Chunk> runningChunks;
-
-    public ChunkTask(XRayPlugin plugin, Player player) {
-        super(TaskType.CHUNK);
-        this.plugin = plugin;
-        this.player = player;
-
-        runningChunks = getRunningChunks();
-
-        this.start();
-    }
-
-    public void run() {
-        throwXray();
-    }
-
-    public void throwXray() {
-        runningChunks.forEach(chunk -> new ChunkThread(plugin, chunk, player).startTask());
-    }
-
-    public void update() {
-        new BukkitRunnable() {
-            public void run() {
-                ArrayList<Chunk> oldChunks = (ArrayList<Chunk>) runningChunks.clone();
-                ArrayList<Chunk> newChunks = getRunningChunks();
-                oldChunks.removeAll(newChunks);
-                runningChunks = newChunks;
-                restore(oldChunks);
-                throwXray();
-            }
-        }.runTaskLaterAsynchronously(plugin, 1);
-    }
-
-    public void restore(List<Chunk> restoring) {
-        new BukkitRunnable() {
-            public void run() {
-                restoring.forEach(chunk -> player.getWorld().refreshChunk(chunk.getX(), chunk.getZ()));
-            }
-        }.runTask(plugin);
-    }
-
-    public ArrayList<Chunk> getRunningChunks() {
-        ArrayList<Chunk> newChunks = new ArrayList<>();
-        Location location = player.getLocation();
-        int beforeRange = plugin.getConfig().getInt("Settings.Range");
-        int finalRange = (beforeRange / 2) * 2 == beforeRange ? beforeRange : beforeRange + 1;
-        for (int x = location.getChunk().getX() - finalRange; x < location.getChunk().getX() + finalRange + 1; x++) {
-            for (int z = location.getChunk().getZ() - finalRange; z < location.getChunk().getZ() + finalRange + 1; z++) {
-                newChunks.add(location.getWorld().getChunkAt(x, z));
-            }
-        }
-        return newChunks;
-    }*/
 }
--- src/main/java/me/kyllian/xRay/threads/ChunkThread.java
+++ src/main/java/me/kyllian/xRay/threads/ChunkThread.java
@@ -3,15 +3,18 @@ package me.kyllian.xRay.threads;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.BlockPosition;
 import com.comphenix.protocol.wrappers.ChunkCoordIntPair;
 import com.comphenix.protocol.wrappers.MultiBlockChangeInfo;
 import com.comphenix.protocol.wrappers.WrappedBlockData;
 import me.kyllian.xRay.XRayPlugin;
+import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 
 public class ChunkThread extends Thread {
 
@@ -21,6 +24,7 @@ public class ChunkThread extends Thread {
 
     private WrappedBlockData barrierBlock;
     private WrappedBlockData airBlock;
+    private ChunkSnapshot snapshot;
 
     public ChunkThread(XRayPlugin plugin, Chunk chunk, Player player) {
         this.plugin = plugin;
@@ -28,21 +32,26 @@ public class ChunkThread extends Thread {
         this.player = player;
         boolean old = Bukkit.getVersion().contains("1.8") || Bukkit.getVersion().contains("1.7");
         barrierBlock = old ? WrappedBlockData.createData(Material.BARRIER, 1) : WrappedBlockData.createData(Material.BARRIER);
-        airBlock =  old ? WrappedBlockData.createData(Material.AIR, 1) : WrappedBlockData.createData(Material.AIR);
+        airBlock = old ? WrappedBlockData.createData(Material.AIR, 1) : WrappedBlockData.createData(Material.AIR);
+
+        snapshot = chunk.getChunkSnapshot();
     }
 
     public void startTask() {
+        if (Bukkit.getVersion().contains("1.16.2") || Bukkit.getVersion().contains("1.16.3")) {
+            startTaskWithNewPackets(); // Yeah.. this should do the job
+            return;
+        }
         PacketContainer packet = new PacketContainer(PacketType.Play.Server.MULTI_BLOCK_CHANGE);
         ChunkCoordIntPair chunkCoords = new ChunkCoordIntPair(chunk.getX(), chunk.getZ());
         MultiBlockChangeInfo[] change = new MultiBlockChangeInfo[65536];
         int i = 0;
-        ChunkSnapshot snapshot = chunk.getChunkSnapshot();
         for (int x = 0; x <= 15; x++) {
             for (int y = 0; y <= 255; y++) {
                 for (int z = 0; z <= 15; z++) {
                     Block foundBlock = chunk.getBlock(x, y, z);
                     Location location = foundBlock.getLocation();
-                    String foundMaterial = snapshot.getBlockType(x, y ,z).toString();
+                    String foundMaterial = snapshot.getBlockType(x, y, z).toString();
                     change[i++] = new MultiBlockChangeInfo(location, plugin.getData().getOrDefault(foundMaterial, barrierBlock));
                 }
             }
@@ -54,5 +63,45 @@ public class ChunkThread extends Thread {
         } catch (InvocationTargetException exception) {
             exception.printStackTrace();
         }
+    }
+
+
+    public void startTaskWithNewPackets() {
+        for (int i = 0; i <= 15; i++) {
+            PacketContainer container = new PacketContainer(PacketType.Play.Server.MULTI_BLOCK_CHANGE);
+            ArrayList<WrappedBlockData> reverseCache = new ArrayList<>();
+            ArrayList<WrappedBlockData> wrappedBlockDataArrayList = new ArrayList<>();
+            ArrayList<Short> shortArrayList = new ArrayList<>();
+            container.getSectionPositions().write(0, new BlockPosition(chunk.getX(), i, chunk.getZ()));
+            int newY = i * 16;
+            for (int x = 0; x <= 15; x++) {
+                for (int y = newY; y <= newY + 15; y++) {
+                    for (int z = 0; z <= 15; z++) {
+                        Block foundBlock = chunk.getBlock(x, y, z);
+                        String foundMaterial = snapshot.getBlockType(x,  y, z).toString();
+                        wrappedBlockDataArrayList.add(plugin.getData().getOrDefault(foundMaterial, barrierBlock));
+                        shortArrayList.add(setShortLocation(foundBlock.getLocation()));
+                    }
+                }
+            }
+            container.getBlockDataArrays().writeSafely(0, wrappedBlockDataArrayList.toArray(new WrappedBlockData[0]));
+            container.getShortArrays().writeSafely(0, ArrayUtils.toPrimitive(shortArrayList.toArray(new Short[0])));
+            try {
+                ProtocolLibrary.getProtocolManager().sendServerPacket(player, container);
+            } catch (InvocationTargetException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    public short setShortLocation(Location loc) {
+        return (setShortLocation(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
+    }
+
+    public short setShortLocation(int x, int y, int z) {
+        x = x & 0xF;
+        y = y & 0xF;
+        z = z & 0xF;
+        return (short) (x << 8 | z << 4 | y << 0);
     }
 }
